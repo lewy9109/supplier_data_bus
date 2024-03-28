1dbe9af0c44b22644d8f424e342a6db187bae52b
krystianlewandowski <lewy9109@gmail.com>
2024-03-28 20:40:28 +0100

    configure doctrine, add migration

---
+++ migrations/Version20240328193805.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240328193805 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE source (id INT AUTO_INCREMENT NOT NULL, method_connection_method VARCHAR(50) NOT NULL, connection_data_login_login VARCHAR(255) DEFAULT NULL, connection_data_password_password VARCHAR(255) DEFAULT NULL, connection_data_host_host VARCHAR(255) DEFAULT NULL, connection_data_port_port VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE source');
+    }
+}
